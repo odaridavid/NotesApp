9e11e77c784a726db831d4cd68d6838aa384cd92
ronnieotieno <ronnieotieno2@gmail.com>
2021-07-03 17:10:17 +0300

    Navigation and Intent session

--- app/src/main/java/com/github/odaridavid/notesapp/MainActivity.kt
+++ app/src/main/java/com/github/odaridavid/notesapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.github.odaridavid.notesapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -10,6 +11,9 @@ import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
+    private var yourThoughtsBody: String? = null
+    private var yourThoughtsTitle: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val thoughtsTitleEditText = findViewById<EditText>(R.id.enterYourThoughtsEditText)
@@ -18,14 +22,24 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         val thoughtsBodyEditText = findViewById<EditText>(R.id.thoughtsBody)
 
         saveYourThoughtsButton.setOnClickListener {
-            val yourThoughtsTitle = thoughtsTitleEditText.text.toString()
-            val yourThoughtsBody = thoughtsBodyEditText.text.toString()
-            if (yourThoughtsTitle.isNotBlank() && yourThoughtsBody.isNotBlank()) {
+            yourThoughtsTitle = thoughtsTitleEditText.text.toString()
+            yourThoughtsBody = thoughtsBodyEditText.text.toString()
+            if (yourThoughtsTitle!!?.isNotBlank() && yourThoughtsBody!!.isNotBlank()) {
                 viewYourThoughtsTextView.text = yourThoughtsTitle + "\n" + yourThoughtsBody
                 viewYourThoughtsTextView.visibility = View.VISIBLE
                 thoughtsBodyEditText.setText("")
                 thoughtsTitleEditText.setText("")
             }
+        }
+
+        viewYourThoughtsTextView.setOnClickListener {
+
+            if (yourThoughtsBody.isNullOrEmpty() || yourThoughtsTitle.isNullOrEmpty()) return@setOnClickListener
+
+            val intent = Intent(this, DetailActivity::class.java)
+            intent.putExtra("title", yourThoughtsTitle)
+            intent.putExtra("body", yourThoughtsBody)
+            startActivity(intent)
         }
 
     }
